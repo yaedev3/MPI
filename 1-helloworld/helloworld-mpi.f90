45ac5c730efd237ac0605220d36eef026c0ee15e
Gerardo <yaehime3@gmail.com>
2019-01-21 21:42:35 -0600

    Cambio de nombre de las carpetas

---
+++ 1-helloworld/helloworld-mpi.f90
@@ -0,0 +1,30 @@
+! File: helloworld-mpi.f90
+!
+! Purpose: 
+! 
+! Input:
+! 
+! Output:
+! 
+! Compile: mpif90 -o helloworld-mpi.o helloworld-mpi.f90
+! 
+! Run: mpiexec -np 10 ./helloworld-mpi.o
+! 
+! Algorithm:
+! 
+! Note:
+
+program name
+implicit none
+include 'mpif.h'
+
+CHARACTER *20 msg
+INTEGER rank, size, ierror, tag, status(MPI_STATUS_SIZE)
+
+call MPI_INIT(ierror)
+call MPI_COMM_SIZE(MPI_COMM_WORLD, size, ierror)
+call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierror)
+print*, 'Hello world! from process ', rank
+call MPI_FINALIZE(ierror)
+    
+end program name
---
+++ 1-helloworld/helloworld.f90
@@ -0,0 +1,22 @@
+! File: helloworld.f90
+!
+! Purpose: 
+! 
+! Input:
+! 
+! Output:
+! 
+! Compile: gfortran -o helloworld.o helloworld.f90
+! 
+! Run: ./helloworld.o
+! 
+! Algorithm:
+! 
+! Note:
+
+program name
+implicit none
+
+write(*, *) 'Hello world! in FORTRAN'
+
+end program name
